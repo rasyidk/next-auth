ddfbe4d019c9152db683060e7f7dc1f31b46e448
rasyidk <rasid.rasid9961@gmail.com>
2023-01-05 20:41:51 +0700

    .

---
+++ pages/register.js
@@ -0,0 +1,15 @@
+import Head from "next/head"
+import Layout from "../layout/layout"
+export default function Register(){
+    return(
+
+        <Layout>
+
+        <Head>
+            <title>Login</title>
+        </Head>
+        <h1>Register</h1>
+        </Layout>
+        
+    )
+}
